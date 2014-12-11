69d68cba1d486fae2a9c93e9ed5daf006c2997a2
Riza Fahmi <rizafahmi@gmail.com>
2014-12-11 22:36:42 +0700

    Add bootstrap. Then refine the layout

--- both/router/routes.js
+++ both/router/routes.js
@@ -14,5 +14,5 @@ Router.configure({
 
 Router.route('/', {name: 'home'});
 Router.route('/student/:user_id/:course_id', {name: 'rooms.index'})
-// Router.route('/chats/:user_id/:course_id', {name: 'chats.index'});
-Router.route('/chats', {name: 'chats.index'});
+Router.route('/chats/:user_id/:course_id', {name: 'chats.index'});
+// Router.route('/chats', {name: 'chats.index'});
