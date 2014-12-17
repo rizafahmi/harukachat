194e8766561d856dce882a1c0ea62f3736b6a11a
Riza Fahmi <rizafahmi@gmail.com>
2014-12-17 18:32:08 +0700

    Grab all students

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,5 +1,23 @@
 var students = Students.find();
 
 if (students.count() < 1) {
-  console.log("Insert student");
+  // TODO: THis is only temporary url
+  var url = "http://117.54.8.205:8080/home/api/get_students";
+  var queryAPI = function () {
+    HTTP.get(url, function (error, result) {
+      if (result) {
+        console.log("Insert student");
+        var data = JSON.parse(result.content);
+        console.log(data.length);
+        for (i=0; i< data.length; i++)
+          Students.insert(data[i]);
+      }
+      else {
+        console.log(error);
+      }
+    });
+  };
+
+  queryAPI();
+
 }
